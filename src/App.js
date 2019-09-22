df7d998dce787c1bc26740e58c95b29ebd533ff9
Kiran Paul VJ <kiranpaul.vj@cognizant.com>
2019-09-22 21:50:23 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+import Switch from '@material-ui/core/Switch';
+import { withStyles } from '@material-ui/core';
+import './switch.css';
 
-function App() {
+const green = '#89d329';
+
+const styles = (theme) => ({
+  root: {
+    height: '45px',
+    width: '70px'
+  },
+  track: {
+    borderRadius: '30px',
+    color: '#cfd7dc',
+    backgroundColor: '#cfd7dc',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color']),
+  },
+  thumb: {},
+  switchBase: {
+    '&$checked': {
+      transform: 'translateX(25px)',
+      '& + $track': {
+        backgroundColor: '#cfd7dc'
+      }
+    },
+    '&$checked:hover': {
+      backgroundColor: '#89d32930'
+    },
+    '&$focusVisible $thumb': {
+      color: '#cfd7dc',
+      border: '6px solid #fff'
+    }
+  },
+  checked: {},
+  focusVisible: {}
+
+});
+function SwitchThumb(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`switch ${props.isChecked ? 'checked':'unchecked'}`}>
+      {props.isChecked ? <CheckIcon /> : <CloseIcon />}
     </div>
   );
 }
 
-export default App;
+function App({ classes }) {
+  const [state, setState] = React.useState({
+    checkedA: true
+  });
+
+  const handleChange = (name) => (event) => {
+    setState({ ...state, [name]: event.target.checked });
+  };
+
+  return (
+    <div style={{ padding: '5em', textAlign: 'center' }}>
+      <Switch
+        icon={<SwitchThumb isChecked={false} />}
+        checkedIcon={<SwitchThumb isChecked={true} />}
+        classes={{
+          root: classes.root,
+          switchBase: classes.switchBase,
+          thumb: classes.thumb,
+          track: classes.track,
+          checked: classes.checked
+        }}
+      />
+    </div>
+  );
+}
+
+export default withStyles(styles)(App);
